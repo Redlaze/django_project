8e8f55ff055e4a1732a5945a71ef1394db16c2c5
orber <dimagold.dg53@gmail.com>
2024-10-15 00:28:54 +0300

    Добавлены категории и изменены отображения их в приложении через шаблоны и url

--- myproject/app/views.py
+++ myproject/app/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect
 
 from .models import *
@@ -25,10 +25,13 @@ menu = [
 
 def index(request):
     posts = Person.objects.all()
+    cats = Category.objects.all()
     context = {
         'posts': posts,
+        'cats': cats,
         'menu': menu,
         'title': 'Главная страница',
+        'cat_selected': 0,
     }
     return render(request, 'app/index.html', context=context)
 
@@ -51,6 +54,23 @@ def login(request):
 
 def show_post(request, post_id):
     return HttpResponse(f"Отображение статьи с id = {post_id}")
+
+def show_category(request, cat_id):
+    posts = Person.objects.filter(cat_id=cat_id)
+    cats = Category.objects.all()
+
+    if len(posts) == 0:
+        raise Http404()
+
+    context = {
+        'posts': posts,
+        'cats': cats,
+        'menu': menu,
+        'title': 'Главная страница',
+        'cat_selected': cat_id,
+    }
+
+    return render(request, 'app/index.html', context=context)
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
