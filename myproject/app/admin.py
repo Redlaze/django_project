a4c2482b60ea499c4212d6423389fd46b314055b
orber <dimagold.dg53@gmail.com>
2025-07-23 01:04:15 +0300

    Настройка админ-панели и добавление файла requirements.txt

--- myproject/app/admin.py
+++ myproject/app/admin.py
@@ -1,16 +1,31 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from .models import *
 
 
 class PersonAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'time_create', 'photo', 'is_published')
+    list_display = ('id', 'title', 'time_create', 'get_html_photo', 'is_published')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'content')
     list_editable = ('is_published',)
     list_filter = ('is_published', 'time_create')
     prepopulated_fields = {"slug": ("title",)}
+    fields = ('title', 'slug', 'cat', 'content', 'photo', 'get_html_photo', 'is_published', 'time_create', 'time_update')
+    readonly_fields = ('time_create', 'time_update', 'get_html_photo')
+    save_on_top = True
 
+    def get_html_photo(self, obj):
+        return_photo = None
+
+        if obj.photo:
+            return_photo = mark_safe('<img src={} width=50>'.format(
+                obj.photo.url,
+            ))
+
+        return return_photo
+
+    get_html_photo.short_description = 'Миниатюра'
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
@@ -21,3 +36,6 @@ class CategoryAdmin(admin.ModelAdmin):
 
 admin.site.register(Person, PersonAdmin)
 admin.site.register(Category, CategoryAdmin)
+
+admin.site.site_title = 'Админ-панель сайта об известных личностях'
+admin.site.site_header = 'Админ-панель сайта об известных личностях'
